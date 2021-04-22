7e34cce13f41233ae74fce3294ba7e18e5174805
kaparoo <kaparoo2001@gmail.com>
2021-04-22 22:23:58 +0900

    :pencil2: Fix typo error of TasNetParam

--- tasnet/param.py
+++ tasnet/param.py
@@ -26,20 +26,19 @@ class TasNetParam:
 
     def save(self, path: str):
         with open(path, "w", encoding="utf8") as f:
-            f.write('\n'.join(f"{key}={value}" for key,
-                              value in self.get_config().items()))
+            f.write('\n'.join(f"{k}={v}" for k, v
+                    in self.get_config().items()))
 
     @staticmethod
     def load(path: str):
-        def convert_num(value):
-            for t in [int, float]:
-                try:
-                    return t(value)
-                except:
-                    pass
+        def convert_int(value):
+            try:
+                return int(value)
+            except:
+                pass
             return value
 
-        def conver_bool(value):
+        def convert_bool(value):
             if value == 'True':
                 return True
             elif value == 'False':
@@ -48,10 +47,10 @@ class TasNetParam:
                 return value
 
         def convert_tup(tup):
-            if tup[0] == "causal":
-                return (tup[0], conver_bool(tup[1]))
+            if tup[0] == 'causal':
+                return (tup[0], convert_bool(tup[1]))
             else:
-                return (tup[0], convert_num(tup[1]))
+                return (tup[0], convert_int(tup[1]))
 
         with open(path, "r", encoding="utf8") as f:
             d = dict(convert_tup(line.strip().split('='))
